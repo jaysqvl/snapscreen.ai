cb9f0b50556e5e8a042b7afdac15cbe9abb53568
jaysqvl <jaysqvl@gmail.com>
2025-04-14 03:10:31 -0700

    started working on the springboot api

---
+++ backend/snapscreen-api/src/main/java/com/snapscreen/snapscreen_api/SnapscreenApiApplication.java
@@ -0,0 +1,13 @@
+package com.snapscreen.snapscreen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapscreenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapscreenApiApplication.class, args);
+	}
+
+}
---
+++ backend/snapscreen-api/src/test/java/com/snapscreen/snapscreen_api/SnapscreenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapscreen.snapscreen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapscreenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
